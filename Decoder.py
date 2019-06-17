4caa1d76ada2c22050e5150aaa9dcbbc7041b856
Xin Zhang <55133006@gilman.ad.mmu.ac.uk>
2019-06-17 18:15:22 +0100

    VV

--- Decoder.py
+++ Decoder.py
@@ -11,7 +11,7 @@ class ConvRelu(nn.Module):
     def __init__(self, in_, out):
         super().__init__()
         self.conv = conv3x3(in_, out)
-        self.activation = nn.ReLU(inplace=True)
+        self.activation = nn.LeakyReLU(negative_slope=0.1, inplace=True)
 
     def forward(self, x):
         x = self.conv(x)
